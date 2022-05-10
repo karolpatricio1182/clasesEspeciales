623ded3f5365a80b9afa013f2e336e7392db6953
Karol Patricio <karol992014@gmail.com>
2022-05-10 10:08:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clasesespeciales/SpecialClasses.kt
@@ -0,0 +1,26 @@
+package com.example.clasesespeciales
+
+object MobyDickWhale{
+    val autor = "Hernan Melville"
+
+    fun jump(){
+        // ...
+    }
+}
+
+enum class Color (val rgb: Int){
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
+sealed class Seal
+
+class SeaLion: Seal()
+class Walrus: Seal()
+
+fun matchSeal(seal: Seal): String{
+    return when (seal){
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
+    }
+}
